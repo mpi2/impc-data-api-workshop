7c280f664343a5f1af75382ce3af964eacf7ee63
dpavam <hhz161@qmul.ac.uk>
2024-10-08 11:19:54 +0100

    build: update setup.py and pyproject.toml settings to include the utils module

--- impc_api_helper/setup.py
+++ impc_api_helper/setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='impc_api_helper',
     version='0.1.0',
     description='A package to facilitate making API request to the IMPC Solr API',
     author='MPI2, Marina Kan, Diego Pava',
     url='https://github.com/mpi2/impc-data-api-workshop',
-    packages=find_packages(),
+    packages=find_packages(include=["impc_api_helper", "impc_api_helper.*"]),
+    include_package_data=True,
     install_requires=[
         'pandas>=2.2.0',
         'requests>=2.31.0',
